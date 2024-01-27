6924bad1fc43c5dedb0e13432191650b041a2565
Arthur Campbell <arfa.camble@gmail.com>
2024-01-27 12:09:30 +0000

    move add to do up to the top and make less obtrusive, logic to mark todo items as completed added in view and app router

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, redirect, render_template, request
-from todo_app.data.session_items import add_item, get_items
+from todo_app.data.session_items import add_item, get_item, get_items, save_item
 
 from todo_app.flask_config import Config
 
@@ -19,4 +19,11 @@ def index():
 def create():
     todo_title = request.form.get('title')
     add_item(todo_title)
+    return redirect('/')
+
+@app.route('/mark_complete/<int:id>', methods=["POST"])
+def mark_complete(id):
+    todo_to_complete = get_item(id)
+    todo_to_complete["status"] = "Completed"
+    save_item(todo_to_complete)
     return redirect('/')
