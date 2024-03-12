7ed8a91375d1fdaf178047e30a14de3d500ddb1b
Arthur Campbell <arfa.camble@gmail.com>
2024-03-12 10:37:29 +0000

    view model updated to pass this test

--- todo_app/models/view_models/progress_view_model.py
+++ todo_app/models/view_models/progress_view_model.py
@@ -1,3 +1,14 @@
 class ProgressViewModel:
   def __init__(self, tasks):
     self._tasks = tasks
+    self.complete = [task for task in tasks if task.complete()]
+
+  @property
+  def complete(self):
+    return self._complete
+
+  @complete.setter
+  def complete(self, complete_tasks):
+    if not isinstance(complete_tasks, list):
+      raise TypeError("Complete Tasks must be a list")
+    self._complete = complete_tasks
