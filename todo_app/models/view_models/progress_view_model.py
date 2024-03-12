e6232b02abc16d607b9d04c91090187c852dd6cb
Arthur Campbell <arfa.camble@gmail.com>
2024-03-12 16:43:55 +0000

    update view model to handle awkward status tasks

--- todo_app/models/view_models/progress_view_model.py
+++ todo_app/models/view_models/progress_view_model.py
@@ -6,12 +6,16 @@ class ProgressViewModel:
   _complete: List[Task]
   _in_progress: List[Task]
   _not_started: List[Task]
+  _tasks_without_status: List[Task]
+  _tasks_with_multiple_stati: List[Task]
 
   def __init__(self, tasks: List[Task]):
     self._tasks = tasks
     self._complete = [task for task in tasks if task.complete()]
     self._in_progress = [task for task in tasks if task.in_progress()]
     self._not_started = [task for task in tasks if task.not_started()]
+    self.tasks_without_status = [task for task in tasks if task.statusless()]
+    self.tasks_with_multiple_stati = [task for task in tasks if task.has_multiple_stati()]
 
   @property
   def complete(self):
@@ -42,3 +46,23 @@ class ProgressViewModel:
     if not isinstance(not_started_tasks, list):
       raise TypeError("Not started tasks must be a list")
     self._not_started = not_started_tasks
+
+  @property
+  def tasks_without_status(self):
+    return self._tasks_without_status
+
+  @tasks_without_status.setter
+  def tasks_without_status(self, tasks_without_status):
+    if not isinstance(tasks_without_status, list):
+      raise TypeError("Not started tasks must be a list")
+    self._tasks_without_status = tasks_without_status
+
+  @property
+  def tasks_with_multiple_stati(self):
+    return self._tasks_with_multiple_stati
+
+  @tasks_with_multiple_stati.setter
+  def tasks_with_multiple_stati(self, tasks_with_multiple_stati):
+    if not isinstance(tasks_with_multiple_stati, list):
+      raise TypeError("Not started tasks must be a list")
+    self._tasks_with_multiple_stati = tasks_with_multiple_stati
